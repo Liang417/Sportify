973c9cec049ddb4de4a06bc61a211abd976adc84
Lewis <jwu417@gmail.com>
2023-11-19 13:40:24 +0800

    feat: Add user signIn/signUp API
    - Build user routes/controllers/models
    - Build multer middleware for file uploads
    - Add simple HTML form for user signIn/signUp

---
+++ middleware/multer.js
@@ -0,0 +1,33 @@
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: async (req, file, cb) => {
+    if (!fs.existsSync('public/uploads')) {
+      fs.mkdirSync('public/uploads');
+    }
+
+    cb(null, 'public/uploads');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024 * 1.5,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('Only image file can be upload'), false);
+    }
+  },
+});
+
+export default upload;
---
+++ models/user.js
@@ -0,0 +1,14 @@
+import pool from './databasePool';
+
+export async function findByEmail(email) {
+  const { rows } = await pool.query('SELECT * FROM "user" WHERE email = $1', [email]);
+  return rows[0];
+}
+
+export async function create(name, email, hashedPassword, avatar) {
+  const { rows } = await pool.query(
+    'INSERT INTO "user"(name, email, password, avatar) VALUES($1,$2,$3,$4) RETURNING id,name,email,avatar',
+    [name, email, hashedPassword, avatar],
+  );
+  return rows[0];
+}
