bc497d2ce3a774e5bfa9f5119fb9942db976fc33
Liang417 <jwu417@gmail.com>
2023-11-30 10:45:54 +0800

    feat: Add homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import ProfilePage from "./pages/ProfilePage";
 import { getUser } from "../../models/activity_user";
 import { useDispatch } from "react-redux";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -19,6 +20,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Homepage />}></Route>
         <Route path="/user/signin" element={<SigninPage />}></Route>
         <Route path="/user/signup" element={<SignupPage />}></Route>
         <Route
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/layout/Header";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Homepage;
